2e0cd65ff1d75b75e15566fce64c2af0d0c0a653
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-21 14:21:49 -0700

    Complete proxy-server skeleton to successfully pipe-forward requests and update component

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const request = require('request');
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static(path.join(__dirname,'../client/public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/stocks/:stock/earnings', (req, res) => {
+  const newUrl = 'http://localhost:3000';
+  request(`${newUrl}/api/stocks/${req.params.stock}/earnings`).pipe(res);
+});
+
+app.get('/stocks/:stock', (req, res) => {
+  res.sendFile(path.join(__dirname,'../client/public/index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
