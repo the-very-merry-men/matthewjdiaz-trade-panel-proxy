85be92254674f9e4e6329cab75ce97ecbb4876a5
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-28 16:52:46 -0700

    Implement layout for proxy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const request = require('request');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 3002;
 
 app.use(express.static(path.join(__dirname,'../client/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/stocks/:stock/earnings', (req, res) => {
   const newUrl = 'http://localhost:3000';
   request(`${newUrl}/api/stocks/${req.params.stock}/earnings`).pipe(res);
+});
+
+app.get('/api/stocks/:stock/price', (req, res) => {
+  const newUrl = 'http://localhost:3001';
+  request(`${newUrl}/api/stocks/${req.params.stock}/price`).pipe(res);
 });
 
 app.get('/stocks/:stock', (req, res) => {
